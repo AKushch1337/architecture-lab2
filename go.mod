c61ad89f9361e2125a9d4052e317c894b0b593a0
Artem Kushch <artemkusch2@gmail.com>
2023-03-17 16:49:46 +0200

    Add implementation of converting postfix expression to prefix

--- go.mod
+++ go.mod
@@ -1,9 +1,7 @@
-// TODO: Переіменуйте на власний модуль.
 module github.com/AKushch1337/architecture-lab2
 
 go 1.20
 
-// TODO: Змініть, якщо вам потрібно використовувати іншу бібліотеку.
 require github.com/stretchr/testify v1.7.0
 
 require (
--- implementation.go
+++ implementation.go
@@ -1,10 +1,59 @@
 package lab2
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
+type Stack []string
 
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func PostfixToPrefix(postfix string) (string, error) {
+	stack := Stack{}
+	tokens := strings.Split(postfix, " ")
+	for _, token := range tokens {
+		if isOperator(token) {
+			op2, err := stack.pop()
+			if err != nil {
+				return "", errors.New("not enough operands for operator")
+			}
+			op1, err := stack.pop()
+			if err != nil {
+				return "", errors.New("not enough operands for operator")
+			}
+			stack.push(fmt.Sprintf("%s %s %s", token, op1, op2))
+		} else {
+			stack.push(token)
+		}
+	}
+	result, err := stack.pop()
+	if err != nil {
+		return "", errors.New("expression is invalid")
+	}
+	if len(stack) > 0 {
+		return "", errors.New("expression is invalid")
+	}
+	return result, nil
+}
+
+func isOperator(token string) bool {
+	switch token {
+	case "+", "-", "*", "/", "^":
+		return true
+	}
+	return false
+}
+
+func (s *Stack) push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) pop() (string, error) {
+	n := len(*s) - 1
+	if n < 0 {
+		return "", errors.New("stack is empty")
+	}
+	val := (*s)[n]
+	*s = (*s)[:n]
+	return val, nil
 }
